889ed685ca0d39041428175735ae250d88a39808
peraza97 <omarperaza19@gmail.com>
2019-02-17 18:07:38 -0800

    reformatted code to make face an object

--- smart_cam.py
+++ smart_cam.py
@@ -29,10 +29,40 @@ class cameraFeed():
     def stop(self):
         self.stopped = True
 
-
 class Face:
-    def __init__(self, dlib_rect):
-        
+    def __init__(self, dlib_rect, img):
+        self.dlib_rect = dlib_rect
+        self.landmarks = self.extract_landmarks(img)
+
+    def extract_landmarks(self, img):
+        return np.matrix([[p.x, p.y] for p in predictor(img, self.dlib_rect).parts()])
+
+    def convert_to_rect(self):
+        pt = self.dlib_rect.tl_corner()
+        pt2 = self.dlib_rect.br_corner()
+        return (pt.x,pt.y, pt2.x-pt.x,pt2.y-pt.y)
+
+    def detect_smile():
+        fts = self.landmarks[48:68]
+        ##do our shit
+
+
+    def draw_face(self,img):
+        (x,y,w,h) = self.convert_to_rect()
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    def draw_landmarks(self, img):
+        for pt in self.landmarks:
+            pos = (pt[0,0], pt[0,1])
+            cv2.circle(img, pos, 2, (0, 255, 255), -1)
+
+    def draw_smile(self,img):
+        fts = self.landmarks[48:68]
+        for pt in fts:
+            pos = (pt[0,0], pt[0,1])
+            cv2.circle(img, pos, 2, (0, 255, 255), -1)
+
+
 
 #CREATE DETECTOR
 detector = dlib.get_frontal_face_detector()
@@ -41,47 +71,17 @@ predictor = dlib.shape_predictor("models/shapes_predict.dat")
 def grab_faces(img):
         return detector(img, 1)
 
-def convert_to_rect(dlib_rect):
-            pt = dlib_rect.tl_corner()
-            pt2 = dlib_rect.br_corner()
-            return (pt.x,pt.y, pt2.x-pt.x,pt2.y-pt.y)
-
-def extract_landmarks(img,faces):
-    my_dict = []
-    for rect in faces:
-        (x,y,w,h) = convert_to_rect(rect)
-        landmarks = np.matrix([[p.x, p.y] for p in predictor(img, rect).parts()])
-        my_dict.append( ((x,y,w,h),landmarks) ) #this stores an array
-        #[ {"rect":points,"landmarsk":points}, {"rect":points,"landmarsk":points} ]
-    return my_dict
-
-def draw_features(img, fts):
-    for pt in fts:
-        pos = (pt[0,0], pt[0,1])
-        cv2.circle(img, pos, 2, (0, 255, 255), -1)
-
-
-
-def draw_face(img, landmarks):
-    for obj in landmarks:
-        (x,y,w,h) = obj[0]
-        points = obj[1]
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        for pt in points:
-            pos = (pt[0,0], pt[0,1])
-            cv2.circle(img, pos, 2, (0, 255, 255), -1)
-
 def main():
     c = cameraFeed().start()
     while True:
         frame = c.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        faces = grab_faces(gray) #returns dliib rectangle for faces
+        dlib_rects = grab_faces(gray) #returns dliib rectangle for faces
 
-        fts = extract_landmarks(gray, faces) #must take in dlib rectangles
+        faces = [Face(rect,frame) for rect in dlib_rects] 
 
-        draw_face(frame,fts)
+
 
         cv2.imshow("Feed", frame)
         k = cv2.waitKey(1)
